290231fae54a09507156ca702b1ac472d6b4377c
AnuragLodhi <anuraglodhi63@gmail.com>
2022-05-09 13:47:50 +0530

    Added main file, updated .gitignore

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Author: Anurag Lodhi May 9, 2022 13:41
+// This is the entry point for this project
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Project Begins";
+    return 0;
+}
